dffe8d5557cd7a1caf2511d6b4a31793763082ad
SchoolDroid <schooldroid@SchoolDroids-MacBook-Air.local>
2018-04-06 14:04:51 +0700

    test few

--- app/src/main/java/net/schooldroid/builder/MainActivity.java
+++ app/src/main/java/net/schooldroid/builder/MainActivity.java
@@ -1,21 +1,42 @@
 package net.schooldroid.builder;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import net.schooldroid.stool.SGps;
 import net.schooldroid.stool.STool;
 
-public class MainActivity extends AppCompatActivity implements STool.locAccessListener{
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity implements STool.locAccessListener {
+
+
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
 
         Button button = findViewById(R.id.mybutt);
         button.setOnClickListener(new View.OnClickListener() {
@@ -28,9 +49,68 @@ public class MainActivity extends AppCompatActivity implements STool.locAccessLi
     }
 
 
-
     @Override
     public void onSuccess() {
-        
+
+
+        new async().execute();
+
+        SGps.reqHighGps(this);
+
+        final SGps sgps = new SGps(MainActivity.this);
+        sgps.on(new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("GPS DEBUG", "" + location.getLatitude() + "," + location.getLongitude() );
+                sgps.off();
+            }
+        });
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!=null) {
+                    Log.d("NEW GPS", location.toString());
+                }
+            }
+        });
+
+
     }
+
+    @SuppressLint("StaticFieldLeak")
+    private class async extends AsyncTask<Void,Void,Void> {
+
+        Map<String,String> result;
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            result =  STool.getNpsnScrap("10496495");
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Sek...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            if (result!=null) {
+                Log.d("Hehehe", String.valueOf(result.entrySet()));
+            }
+        }
+    }
+
+
 }
--- stool/src/main/java/net/schooldroid/stool/STool.java
+++ stool/src/main/java/net/schooldroid/stool/STool.java
@@ -7,7 +7,13 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 
-import net.schooldroid.stool.ReqLocationActivity;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class STool {
@@ -30,5 +36,36 @@ public class STool {
     public static boolean checkLocationPermission(Context context) {
         return Build.VERSION.SDK_INT < 23 || ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
+
+    public static Map<String,String> getNpsnScrap(String npsn) {
+        String baseUrl = "http://referensi.data.kemdikbud.go.id/tabs.php?npsn=";
+        String urlToScrap = baseUrl + npsn;
+        try {
+            Document document = Jsoup.connect(urlToScrap).get();
+            Elements elements = document.select("#tabs-1 table tbody tr td:first-child table tbody tr td:nth-child(4)"); //#box-table-a tbody tr:first-child td
+
+            Map<String,String> dict = new HashMap<>();
+
+            dict.put("nama", elements.get(0).text());
+            dict.put("npsn",elements.get(1).text());
+            dict.put("alamat",elements.get(2).text());
+            dict.put("kode_pos",elements.get(3).text());
+            dict.put("kel",elements.get(4).text());
+            dict.put("kec",elements.get(5).text());
+            dict.put("kab",elements.get(6).text());
+            dict.put("prov",elements.get(7).text());
+            dict.put("status",elements.get(8).text());
+            dict.put("waktu",elements.get(9).text());
+            dict.put("jenjang",elements.get(10).text());
+
+            return dict;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
 
 }
